71abbb161f85f988823126d8e821b54f65047204
zhangwei <zw122618@126.com>
2018-07-22 22:04:54 +0800

    init Safety
    Safety is important

---
+++ CollectionSafety/CollectionSafety/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionSafety
+//
+//  Created by MLD on 2018/7/19.
+//  Copyright © 2018年 MLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionSafety/CollectionSafetyTests/CollectionSafetyTests.m
@@ -0,0 +1,45 @@
+//
+//  CollectionSafetyTests.m
+//  CollectionSafetyTests
+//
+//  Created by MLD on 2018/7/19.
+//  Copyright © 2018年 MLD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "CollectionSafety.h"
+@interface CollectionSafetyTests : XCTestCase
+
+@end
+
+@implementation CollectionSafetyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testNSArraySafe {
+    NSString *str = nil;
+    NSArray *arr1 = @[str];
+    
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
